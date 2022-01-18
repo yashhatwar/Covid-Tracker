53769209045bf37bf7079b5dc244fa1a66d3345e
Yash Hatwar <yashhatwar40@gmail.com>
2022-01-18 15:53:32 +0530

    "code before npm i  axios"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CardGroup>
+  <Card bg="primary" text="white" className='text-center' style={{margin : "10px"}}>
+   
+    <Card.Body>
+      <Card.Title>Cases</Card.Title>px
+      <Card.Text>
+       100
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small>Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card bg="danger" text="white" className='text-center' style={{margin : "10px"}}>
+   
+    <Card.Body>
+      <Card.Title>Deaths</Card.Title>
+      <Card.Text>
+       0
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small >Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card bg="success" text="white" className='text-center' style={{margin : "10px"}}>
+  
+    <Card.Body>
+      <Card.Title>Recovered</Card.Title>
+      <Card.Text>
+        99
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small >Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
