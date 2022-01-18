d1d5c84444208a953649ecfeda2f51fb0b8751d6
Yash Hatwar <yashhatwar40@gmail.com>
2022-01-18 16:49:09 +0530

    "API used"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Card from 'react-bootstrap/Card'
 import CardGroup from 'react-bootstrap/CardGroup'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from "axios"
 
 function App() {
+
+  const[latest,setLatest] = useState("");
+
+  useEffect(() =>{
+    axios
+    .get("https://corona.lmao.ninja/v3/covid-19/all")
+    .then(res => {
+      setLatest(res.data);
+    })
+    .catch(err =>{
+      console.log(err);
+    });
+  },[]);
+
+  const date = new Date(parseInt(latest.updated));
+  const lastUpdated = date.toString();
   return (
     <div>
       <CardGroup>
   <Card bg="primary" text="white" className='text-center' style={{margin : "10px"}}>
    
     <Card.Body>
-      <Card.Title>Cases</Card.Title>px
+      <Card.Title>Cases</Card.Title>
       <Card.Text>
-       100
+       {latest.todayCases}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small>Last updated 3 mins ago</small>
+      <small>Last updated {lastUpdated}</small>
     </Card.Footer>
   </Card>
   <Card bg="danger" text="white" className='text-center' style={{margin : "10px"}}>
@@ -24,11 +41,11 @@ function App() {
     <Card.Body>
       <Card.Title>Deaths</Card.Title>
       <Card.Text>
-       0
+       {latest.todayDeaths}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small >Last updated 3 mins ago</small>
+      <small >Last updated  {lastUpdated}</small>
     </Card.Footer>
   </Card>
   <Card bg="success" text="white" className='text-center' style={{margin : "10px"}}>
@@ -36,11 +53,11 @@ function App() {
     <Card.Body>
       <Card.Title>Recovered</Card.Title>
       <Card.Text>
-        99
+        {latest.todayRecovered}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small >Last updated 3 mins ago</small>
+      <small >Last updated  {lastUpdated}</small>
     </Card.Footer>
   </Card>
 </CardGroup>
